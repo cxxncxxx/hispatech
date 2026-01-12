5f9aa0e59cfcfb1bbf3c16ebb81a98fa226ec361
CXXN <rifinaxd@gmail.com>
2026-01-12 16:20:27 -0300

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -19,17 +19,44 @@ const products = {
         "https://ae-pic-a1.aliexpress-media.com/kf/S68d3323ee96549e1b134cb0e7671c3234.jpg_220x220q75.jpg_.avif",
         "https://ae-pic-a1.aliexpress-media.com/kf/S648bbffb285b46dbb1161bf9e863b0d6P.jpg_220x220q75.jpg_.avif"
       ]
+    },
+    {
+      name: "Xiaomi POCO C85 256GB 8GB RAM",
+      price: 110,
+      images: [
+        "https://imgs.search.brave.com/VQAl4ZRKPJYwIX6aaTrUL7gHPxKAl-JlIt15d2CbVWc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/d2lyZWxlc3NwbGFj/ZS5jb20vY2RuL3No/b3AvZmlsZXMvMl9l/OWMzOWQ5YS05MDRl/LTRhNzQtYmRmOC1i/OWIzZDUzNDU3ZmJf/NzAweDcwMC5wbmc_/dj0xNzU4ODM1ODYy",
+        "https://ae-pic-a1.aliexpress-media.com/kf/S8827513e8eeb4ceda1f71eb9a86167dcG.jpg_220x220q75.jpg_.avif",
+        "https://ae-pic-a1.aliexpress-media.com/kf/S45b4b38d2cc34195b76ae1d860d662692.jpg_220x220q75.jpg_.avif"
+      ]
+    },
+    {
+      name: "Xiaomi Redmi Note 14 Pro 5G 256GB 8GB RAM",
+      price: 250,
+      images: [
+        "https://ae-pic-a1.aliexpress-media.com/kf/Sa98a9e1014cb40018036fe7566947e198.png_220x220.png_.avif",
+        "https://ae-pic-a1.aliexpress-media.com/kf/S8ec64b34968141d99c00bf51d3b4887e4.png_220x220.png_.avif"
+      ]
     }
   ],
 
   Samsung: [
+    {
+      name: "Samsung Galaxy A56 5G 256GB 8GB RAM",
+      price: 390,
+      images: [
+        "https://ae-pic-a1.aliexpress-media.com/kf/E810419646ec2469b8156a0f4280b14edT.png_220x220.png_.avif",
+        "https://ae-pic-a1.aliexpress-media.com/kf/E52efa0e3f6824ce69c41b41e1e6f1379i.png_220x220.png_.avif"
+      ]
+    },
     {
       name: "Samsung Galaxy S25 Ultra 256GB 12GB RAM",
       price: 1200,
       images: [
         "https://ae-pic-a1.aliexpress-media.com/kf/E8ac20bd556134f9795116bed4a80473dp.jpg_220x220q75.jpg_.avif",
         "https://ae-pic-a1.aliexpress-media.com/kf/E3fcb3083283342e9a87f7a54bb0389071.jpg_220x220q75.jpg_.avif",
-        "https://ae-pic-a1.aliexpress-media.com/kf/Ebe97124dcfe64775bceb14a426205950d.jpg_220x220q75.jpg_.avif"
+        "https://ae-pic-a1.aliexpress-media.com/kf/Ebe97124dcfe64775bceb14a426205950d.jpg_220x220q75.jpg_.avif",
+        "https://ae-pic-a1.aliexpress-media.com/kf/Ed51f49a60e8e4512aab8a5cbf4161bbfH.jpg_220x220q75.jpg_.avif",
+        "https://ae-pic-a1.aliexpress-media.com/kf/E225a4b8dd94846428e1071cd5d03b2b2a.jpg_220x220q75.jpg_.avif"
       ]
     }
   ],
@@ -41,7 +68,25 @@ const products = {
       images: [
         "https://ae-pic-a1.aliexpress-media.com/kf/S57b1525a768f44578809b93bcffd5060b.jpg_220x220q75.jpg_.avif",
         "https://ae-pic-a1.aliexpress-media.com/kf/Sf29f6954e245410aa2222f8812666e2fd.jpg_220x220q75.jpg_.avif",
-        "https://ae-pic-a1.aliexpress-media.com/kf/S8d7032d9b7104936a23771e727400081Z.jpg_220x220q75.jpg_.avif"
+        "https://ae-pic-a1.aliexpress-media.com/kf/S8d7032d9b7104936a23771e727400081Z.jpg_220x220q75.jpg_.avif",
+        "https://ae-pic-a1.aliexpress-media.com/kf/S83b683f4d09c459693a3fa81034f8d8bE.png_220x220.png_.avif",
+        "https://ae-pic-a1.aliexpress-media.com/kf/S40f93b47d7aa420a89cd9c9433dd2a00j.jpg_220x220q75.jpg_.avif"
+      ]
+    },
+    {
+      name: "iPhone 12 Pro 128GB 6GB RAM",
+      price: 350,
+      images: [
+        "https://ae-pic-a1.aliexpress-media.com/kf/S7ed7f8abfb874ba991b6476411cdbd69i.jpg_220x220q75.jpg_.avif"
+      ]
+    },
+    {
+      name: "iPhone 14 Pro 256GB 6GB RAM",
+      price: 660,
+      images: [
+        "https://ae-pic-a1.aliexpress-media.com/kf/S3126c0a24d2e4acb89a8bce298f907e2E.jpg_220x220q75.jpg_.avif",
+        "https://ae-pic-a1.aliexpress-media.com/kf/S7804e0b998074b93aef97ed3daa93cd5u.png_220x220.png_.avif",
+        "https://ae-pic-a1.aliexpress-media.com/kf/Sf0e386d385e24ee8bfde24c8fbef04621.png_220x220.png_.avif"
       ]
     }
   ]
@@ -52,28 +97,23 @@ function renderProducts(brand, containerId) {
   if (!container) return;
 
   products[brand].forEach((product, index) => {
+    const mainImgId = `${brand}-${index}-main`;
+
     const card = document.createElement("div");
     card.className = "product-card";
-
-    const mainImgId = `${brand}-${index}-main`;
 
     card.innerHTML = `
       <img id="${mainImgId}" src="${product.images[0]}" class="main-img">
 
       <div class="thumbs">
         ${product.images
-          .map(
-            img =>
-              `<img src="${img}" onclick="changeImage('${mainImgId}', '${img}')">`
-          )
+          .map(img => `<img src="${img}" onclick="changeImage('${mainImgId}', '${img}')">`)
           .join("")}
       </div>
 
       <h3>${product.name}</h3>
       <p class="price">€${product.price}</p>
-      <button onclick="buyProduct('${brand}', '${product.name}', ${product.price})">
-        Comprar
-      </button>
+      <button onclick="buyProduct('${brand}', '${product.name}', ${product.price})">Comprar</button>
     `;
 
     container.appendChild(card);
@@ -87,12 +127,3 @@ window.changeImage = (id, src) => {
 renderProducts("Xiaomi", "xiaomiProducts");
 renderProducts("Samsung", "samsungProducts");
 renderProducts("Apple", "appleProducts");
-
-window.buyProduct = (brand, name, price) => {
-  document.getElementById("productBrand").value = brand;
-  document.getElementById("productName").value = name;
-  document.getElementById("price").value = price;
-
-  document.getElementById("orderSection").style.display = "block";
-  window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
-};
